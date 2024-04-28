aba72b5bb820c94cd8c08a231cfa13d4185df1d2
Samuel <Samueladeyemi2006@gmail.com>
2024-04-28 14:15:59 +0100

    added style prop to button

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import styles from "./button.module.css";
 
-export const Button = ({ children, type, onClick, href }: any) => {
+export const Button = ({ children, type, style, onClick, href }: any) => {
   return (
     <div>
       <a href={href} target="_blank" style={{ textDecoration: "none" }}>
         {" "}
-        <button className={styles.button} type={type} onClick={onClick}>
+        <button
+          className={styles.button}
+          style={style}
+          type={type}
+          onClick={onClick}
+        >
           {children}
         </button>
       </a>

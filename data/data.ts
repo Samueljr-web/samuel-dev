5348fe3418b53c5ba88426bb2907fbdf5d35008a
Samuel <Samueladeyemi2006@gmail.com>
2024-08-06 05:35:02 +0100

    modified projectdetails data

--- data/data.ts
+++ data/data.ts
@@ -8,7 +8,7 @@ interface projectsDataType {
   id: string;
   name: string;
   description: string;
-  stack: string;
+  stack: string[];
   link: string;
   github: string;
 }
@@ -16,98 +16,30 @@ interface projectsDataType {
 export const projectsData: projectsDataType[] = [
   {
     id: "1",
-    name: "Portfolio Website",
-    description: "This is the website you are currently viewing",
-    stack: "Reactjs + Gsap ",
-    link: "/",
-    github: "",
+    name: "Cert go",
+    description:
+      "A webapp that allows users create, share and download amazing single or bulk certificates within split seconds and in any format of your choice",
+    link: "https://certgo.app",
+    stack: ["Reactjs", "Mongodb", "Nodejs", "Expressjs"],
+    github: "https://github.com/samueljr-web",
   },
+
   {
     id: "2",
     name: "Meta bnb",
     description:
       "A task assigned to me at hngi9 to implement a website that was designed in figma",
-    stack: "Reactjs + Tailwindcss + Aos",
-    link: "",
+    stack: ["Reactjs", "Tailwindcss", "Aos"],
+    link: "/",
     github: "https://github.com/samueljr-web/meta-bnb",
   },
   {
     id: "3",
-    name: "Countdown Timer",
+    name: "Lets countdown",
     description:
       "A webapp that allows users to create countdowns for thier events fast, easy and quick",
-    stack: "Reactjs + Tailwindcss + Nodejs + Expressjs",
+    stack: ["Reactjs", "Tailwindcss", "Nodejs", "Expressjs"],
     link: "https://countdown-app-chi.vercel.app/",
-    github: "https://github.com/samueljr-web",
-  },
-  {
-    id: "11",
-    name: "Cert go",
-    description:
-      "A webapp that allows users create, share and download amazing single or bulk certificates within split seconds and in any format of your choice",
-    link: "certgo.app",
-    stack: "Reactjs, Mongodb, Nodejs, Express Js",
-    github: "https://github.com/samueljr-web",
-  },
-
-  {
-    id: "4",
-    name: "UTY - Landing page",
-    description: "I created a landing page for a delivery service",
-    stack: "Javacript + Css",
-    link: "https://uty.netlify.app/#",
-    github: "https://github.com/samueljr-web",
-  },
-  {
-    id: "5",
-    name: "F.D.M.A",
-    description: " This is a school website and my fisrt next.js project.",
-    stack: "Nextjs + Tailwindcss + AOS",
-    link: "https://fdma-website.vercel.app",
-    github: "",
-  },
-  {
-    id: "6",
-    name: "Food City ",
-    description: "A restaurant website",
-    stack: "Reactjs + Css",
-    link: "https://restaurant-website-silk.vercel.app",
-    github: "",
-  },
-
-  {
-    id: "7",
-    name: "Twitter Clone",
-    description:
-      " This was my first Reactjs project/challenge i hopped on, i cloned twitter.",
-    stack: "Reactjs + Sass",
-    link: "https://react-twitterclone.netlify.app/",
-    github: "",
-  },
-  {
-    id: "8",
-    name: "Todo App",
-    description: "A todo app..",
-    stack: "Javacript + Css",
-    link: "https://youthful-lumiere-d7d69b.netlify.app/",
-    github: "",
-  },
-
-  {
-    id: "9",
-    name: "Personal Website v1",
-    description: "This was my former personal/portfolio website.",
-    stack: "Reactjs + Css",
-    link: "https://devsamuel.vercel.app/",
-    github: "",
-  },
-
-  {
-    id: "10",
-    name: "CV",
-    description: "A cv desgined with css",
-    stack: "Reactjs + Css",
-    link: "https://sh-cv.netlify.app",
     github: "https://github.com/samueljr-web",
   },
 ];

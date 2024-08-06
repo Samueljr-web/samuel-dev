b7b9e05073fce663abcecc5ca2ed0ba867e3c41c
Samuel <Samueladeyemi2006@gmail.com>
2024-08-06 05:32:26 +0100

    Refactor: Move sections to separate pages

---
+++ pages/projects.tsx
@@ -0,0 +1,51 @@
+import { projectsData } from "@/data/data";
+import Image from "next/image";
+import React from "react";
+
+export default function Projects() {
+  return (
+    <div>
+      <h2 className="font-medium text-2xl text-white">Featured projects</h2>
+
+      {projectsData.map((project: any) => (
+        <div key={project} className="mt-4 space-y-2">
+          <h2 className="text-white font-medium text-[1.3rem]">
+            {project.name}
+          </h2>
+          <p>{project.description}</p>
+          <div className="flex justify-between">
+            <div>
+              {project.stack.map((tech: string, index: number) => (
+                <span
+                  key={index}
+                  className="w-auto lowercase bg-[#ffffff1a] h-[32px] px-2 py-1 mr-2 "
+                >
+                  {tech}
+                </span>
+              ))}
+            </div>
+
+            <div className="flex gap-2">
+              <a href={project.link} target="_blank">
+                <Image
+                  src="/assets/external-link.svg"
+                  alt=""
+                  width={20}
+                  height={20}
+                />
+              </a>
+              <a href={project.link} target="_blank">
+                <Image
+                  src="/assets/github-line.svg"
+                  alt=""
+                  width={20}
+                  height={20}
+                />
+              </a>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
---
+++ pages/skills.tsx
@@ -0,0 +1,76 @@
+import React from "react";
+import { skillsData } from "@/data/data";
+import Image from "next/image";
+
+export default function Skills() {
+  return (
+    <div>
+      {" "}
+      <h2 className="font-medium text-2xl text-white">Tools</h2>
+      <div className="mt-2 flex flex-col items-center">
+        <div className="flex">
+          {skillsData.slice(0, 3).map((skill) => (
+            <div
+              key={skill.id}
+              className="flex w-[170px] h-11 p-2 m-4 gap-7 items-center bg-[#ffffff1a] rounded-[10px]"
+            >
+              <Image src={skill.icon} alt="" width={30} height={30} />
+              <h3>{skill.name}</h3>
+            </div>
+          ))}
+        </div>
+        <div className="flex">
+          {skillsData.slice(3, 7).map((skill) => (
+            <div
+              key={skill.id}
+              className="flex w-[170px] h-11 items-center  gap-7 m-4 p-2 bg-[#ffffff1a] rounded-[10px]"
+            >
+              <Image src={skill.icon} alt="" width={30} height={30} />
+              <h3>{skill.name}</h3>
+            </div>
+          ))}
+        </div>
+        <div className="flex">
+          {skillsData.slice(7, 10).map((skill) => (
+            <div
+              key={skill.id}
+              className="flex w-[170px] h-11 items-center  p-2 m-4 gap-7 bg-[#ffffff1a] rounded-[10px] "
+            >
+              <Image src={skill.icon} alt="" width={30} height={30} />
+              <h3>{skill.name}</h3>
+            </div>
+          ))}
+        </div>
+        <div className="flex">
+          {skillsData.slice(10, 11).map((skill) => (
+            <div
+              key={skill.id}
+              className="flex w-[170px] h-11 items-center  gap-7 m-4 p-2 bg-[#ffffff1a] rounded-[10px] "
+            >
+              <Image src={skill.icon} alt="" width={30} height={20} />
+              <h3>{skill.name}</h3>
+            </div>
+          ))}
+        </div>
+      </div>
+      <section className="hidden">
+        <h2>Tools</h2>
+        <div>
+          {Array.from({ length: Math.ceil(skillsData.length / 2) }).map(
+            (_, index) => (
+              <div className="" key={index}>
+                {/* Map over each pair of skills and render them */}
+                {skillsData.slice(index * 2, index * 2 + 2).map((skill) => (
+                  <div key={skill.id} className="">
+                    <Image src={skill.icon} alt="" width={30} height={30} />
+                    <h3>{skill.name}</h3>
+                  </div>
+                ))}
+              </div>
+            )
+          )}
+        </div>
+      </section>
+    </div>
+  );
+}
